4a840591873496f08dc0a8965d06925ef52bf5a0
nandakhoerunissa <109189382+nandakhoerunissa@users.noreply.github.com>
2022-10-06 08:34:10 +0700

    Add files via upload

---
+++ penjualannanda.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2022 at 03:31 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualannanda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(4) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `stok` int(100) NOT NULL,
+  `harga` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `stok`, `harga`) VALUES
+(4, 'roti', 5, 25000),
+(5, 'roti', 5, 30000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- proses-tambah.php
+++ proses-tambah.php
@@ -1,11 +1,11 @@
 <?php
 include("koneksi.php");
 if(isset($_POST['tambah'])){
-    $nama=$_POST['nama'];
+    $nama=$_POST['barang'];
     $stok=$_POST['stok'];
     $harga=$_POST['harga'];
 
-    $sql = "INSERT INTO barang(nama, stok, harga) VALUES ('$nama', '$stok', '$harga')";
+    $sql = "INSERT INTO barang(nama_barang, stok_barang, harga_barang) VALUES ('$nama', '$stok', '$harga')";
     $query = mysqli_query($db, $sql);
 
     if($query){
